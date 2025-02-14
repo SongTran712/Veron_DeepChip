376c6da47b413af9a99d26cfe1695a3ed3eed58d
Song812ads <songtran91105712@gmail.com>
2025-02-14 21:01:07 +0700

    update

--- split.py
+++ split.py
@@ -1,82 +1,101 @@
 import pdfplumber
 import re
 from langchain_core.vectorstores import InMemoryVectorStore
-from langchain_ollama import ChatOllama
-# from langchain_community.document_loaders import TextLoader
-from langchain_community.embeddings import OllamaEmbeddings
+from langchain.embeddings import OllamaEmbeddings
+from langchain.chat_models import ChatOllama
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.vectorstores import FAISS
+from langchain_core.runnables import RunnablePassthrough
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import pdfplumber
+from langchain import hub
+from langchain_community.vectorstores import Chroma
+from langchain.document_loaders import PDFPlumberLoader
+from langchain_core.output_parsers import StrOutputParser
 
-
+def chunk_content(content, chunk_size=500, overlap=0):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size,
+        chunk_overlap=overlap,
+        separators=["\n", " ", ""]
+    )
+    return text_splitter.split_text(content)
 
 PROMPT_TEMPLATE = """
-
 You are an expert research assistant. Use the provided context to answer the query. 
-If unsure, state that you don't know. Be concise and factual (max 3 sentences).
-
 Query: {user_query} 
 Context: {document_context} 
 Answer:
 """
 
-
 def starts_with_number_dot(s):
-    pattern = r"^\d+(?:\.\d+){0,2}\.\s+\S.*"
+    pattern = r"^\d+(?:\.\d+){0,3}\s+\S.*"
     return bool(re.match(pattern, s))
 
 def check_session(pdf_path):
     session = []
     topic = ""
     content = ""
+    keep = False
     with pdfplumber.open(pdf_path) as pdf:
-        for page_number in range(5,7):
+        for page_number in range(len(pdf.pages)):
             page = pdf.pages[page_number]
             text = page.extract_text()
             lines = text.split("\n")
             for index, line in enumerate(lines):
                 if index != 0 and index !=1 and index!= len(lines)-2 and index!=len(lines) - 1:
-                    # print(line)
-                    if index == 2 and not starts_with_number_dot(line):
-                        content = content+line+"\n"
-                    if starts_with_number_dot(line):
-                        # topic = line
-                        session.append(f"Topic: {topic}\nContent: {content}")
+                    if starts_with_number_dot(line) and keep == False:
+                        session.append(f"{topic}\n{content}")
                         topic = line
                         content = ""
+                        keep = True
+                    elif starts_with_number_dot(line) and keep == True:
+                        topic = ""
+                        content = content + line + "\n"
+                        keep = True 
                     else:
+                        keep = False
                         content = content + line +"\n"
         return session
-
-# Example usage
+from langchain_core.documents import Document
 pdf_path = "./code.pdf"
-  # Matches "a.b.c" format
-
 content = check_session(pdf_path)
-
-embeddings = OllamaEmbeddings(model = 'deepseek-r1:1.5b')
-
+docs = []
+for index, cont in enumerate(content):
+    doc = Document(id=str(index+1), page_content = cont)
+    docs.append(doc)
+    
+embeddings = OllamaEmbeddings(model = "deepseek-r1:1.5b")
 llm = ChatOllama(
-    model = "deepseek-r1:1.5b"
-    # temperature = 0.8,
-    # num_predict = 256,
+    model = "deepseek-r1:1.5b",
+    temperature = 0,
 )
 
 vector_store = InMemoryVectorStore(embeddings)
+vector_store.add_documents(documents = docs)
+retriever = vector_store.as_retriever(
+    search_type="mmr",
+    search_kwargs={"k": 5, "fetch_k": 10, "lambda_mult": 0.5},
+)
 
-vector_store.add_texts(content)
+template = """Answer the question based only on the following context:
+{context}
 
+Question: {question}
+"""
 
-def generate_answer(user_query, context_documents):
-  context_text = "\n\n".join([doc.page_content for doc in context_documents])
-  conversation_prompt = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
-  response_chain = conversation_prompt | llm
-  return response_chain.invoke({"user_query": user_query, "document_context": context_text})
+prompt = ChatPromptTemplate.from_template(template)
 
-def find_related_documents(query):
-  return vector_store.similarity_search(query)
-  
-# ai_response = generate_answer("")
-print("finding related...")
-docs = find_related_documents("What is PE1000N?")
-print("gen...")
-output = generate_answer("What is PE1000N?", docs)
-print(output.content)
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+rag_chain = (
+    {"context": retriever | format_docs, "question": RunnablePassthrough()}
+    | prompt
+    | llm
+    | StrOutputParser()
+)
+    # return rag_chain.invoke(user_query)
+
+output = rag_chain.invoke("if else condition structure")
+print(output)
